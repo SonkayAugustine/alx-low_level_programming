48fb63a1c6b339a8633cb7df4373f875760f5041
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-21 05:21:05 -0700

    niow

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main- prints sizeof data types on the computer
+ *Warnings are allowed
+ *Return zero or otherwise no-zero
+ */
+
+int main(void)
+{
+for size of char
+	   printf("Size of a char: %dbyte(s)", sizeof(char));
+return (0);
+}
