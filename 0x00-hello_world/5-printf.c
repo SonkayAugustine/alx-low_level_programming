b750cac37f18aa3a9880c09fc288e29d19caa60d
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-21 04:04:29 -0700

    now

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - prints the phrase "with proper grammar, but the outcome is a piece of art"
+ *main - prints the phrase "with proper grammar,piece of art"
  *followed by a new line
  *Return: 0 if exited properly, non-zero otherwise
  */
