b5307682461b77751ec9ae6c96bb28739077d0ba
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-21 06:10:52 -0700

    now

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main- prins the  quote "Art is useful"
+ *Warnings are allowed
+ *Return zero or otherwise no-zero
+ */
+int write(int filedes, const char *buf unsigned int byte);
+int main(int argc, char** argv)
+{
+  write(1, "and that piece of art is useful'"' - Dora Korpar, 2015-10-19\n", 27);
+return (1);
+}
