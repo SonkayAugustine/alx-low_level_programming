b2ebb8df75b20e6f262c54ebcf2fb75a585ec433
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-21 11:13:59 -0700

    now

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints the  quote "Art is useful"
- *
- * Return: 1
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 
 int main(void)
-{
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
 return (1);
 }
