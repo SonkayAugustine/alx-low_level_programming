7e280023a605a75934a3f036873c89faab29910b
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-22 09:07:18 -0700

    now

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,20 +7,17 @@
  * followed by a new line
  * Return: Always 0 (success)
  */
-
 int _putchar(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
 
 return (0);
 }
