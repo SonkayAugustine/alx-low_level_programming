ae7de65db9fd57cbc35445ec11d54c251384c45f
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-22 10:00:34 -0700

    now

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _putchar - Print the word "holberton"
+ * main - Print the word "holberton"
  * followed by a new line
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 _putchar('H');
