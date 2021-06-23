482cd7d227fba3009a1be42861ebf21b4e4c0ad8
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-23 06:27:20 -0700

    now

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+return (0);
+}
