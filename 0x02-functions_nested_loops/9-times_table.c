4904e9c48db195dc3e58faf500c43a68c752074b
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-23 11:07:26 -0700

    now

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+int i, j, product, product_first_digit, product_last_digit;
+for (i = 0; i <= 9; j++)
+{
+product = i * j;
+product_first_digit = product / 10;
+product_last_digit = product % 10;
+
+if (j != 0)
+{
+_putchar(product_first_digit == 0 ? ' ' : product_first_digit + '0');
+_putchar(product_last_digit + '0');
+if (j == 9)
+_putchar('\n');
+else
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+}
