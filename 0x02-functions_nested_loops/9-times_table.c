0a8d0ae983f1e2351a25223b0c9e44d9e3f08efe
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-23 11:56:40 -0700

    now

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,9 @@
 void times_table(void)
 {
 int i, j, product, product_first_digit, product_last_digit;
-for (i = 0; i <= 9; j++)
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j < 9; i++)
 {
 product = i * j;
 product_first_digit = product / 10;
@@ -21,6 +23,7 @@ _putchar(product_first_digit == 0 ? ' ' : product_first_digit + '0');
 _putchar(product_last_digit + '0');
 if (j == 9)
 _putchar('\n');
+}
 else
 {
 _putchar(',');
@@ -28,4 +31,5 @@ _putchar(' ');
 }
 }
 }
+_putchar('\n');
 }
