71cd4eb5ca2c90f84fa6434aa0f1b3edc15781e5
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-24 13:41:45 -0700

    now

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,7 +4,7 @@
 /**
  * print_remaining_days - takes a date and prints how many days are left int the year, taking leaps into a * ccount
  * @month: month in number format
- * @ day: day of month
+ * @day: day of month
  * @year: year
  * return: void
  */
