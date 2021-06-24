3b6619f9afce87ac8093141d966285330dba524a
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-24 09:36:00 -0700

    now

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,12 +9,13 @@ int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n");
-i = 0;
 /**
+ * i = 0; - integer to be checked
  * while (i < 10)
  * {
  * putchar(i);
  * }
+ * \\o/\n" - new line
  */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
