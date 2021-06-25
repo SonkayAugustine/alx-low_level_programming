4d8d7502f9652cad04f2822b8b5ccfdaf096b5ae
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-25 10:03:02 -0700

    now

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: integer
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+int row;
+int col;
+
+if (n <= 0)
+_putchar('\n');
+for (row = 1; row <= n; row++)
+{
+for (col = 1; col < row; col++)
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
