85a368644d0237ff13160fed257ad5840a4066c4
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-25 12:00:20 -0700

    now

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 int num;
 for (num = 1; num <= 100; num++)
 printf(" ");
-if (num % 15 == 0)
+if (num % 3 && num % 5 == 0)
 printf("FizzBuzz");
 else if (num % 5 == 0)
 printf("Buzz");
