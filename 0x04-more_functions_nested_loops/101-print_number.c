b2ddf74b72ab6593a4fe5238d59889a160266eb3
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-25 11:43:48 -0700

    now

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_number - Entry point
+ * @n: integer
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n == 0)
+_putchar('0');
+
+if (n / 10)
+printnumber(n / 10);
+_putchar(n%10 + '0');
+}
