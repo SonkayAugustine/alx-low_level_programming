52db9e830bae303838e3635eeabd94436c35b314
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-25 13:44:30 -0700

    now

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,13 +8,22 @@
 
 void print_triangle(int size)
 {
-int i, j;
+int i;
+int j;
+int k;
+
 if (size <= 0)
 _putchar('\n');
 for (i = 1; i <= size; i++)
 {
 for (j = 0; j < size; j++)
-_putchar(j <= (size - i - 2) ? ' ' : '#');
+{
+_putchar(' ');
+}
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}
 _putchar('\n');
 }
 }
