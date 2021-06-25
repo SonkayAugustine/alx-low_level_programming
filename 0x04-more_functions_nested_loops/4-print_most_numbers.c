2111fba79263826c464bed678824e3b6e8ed92fc
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-25 09:22:07 -0700

    now

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: always 0.
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+if (i == 2 || i == 4)
+continue;
+_putchar(i);
+}
+_putchar('\n');
+}
