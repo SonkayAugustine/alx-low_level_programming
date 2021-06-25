3c8daa71aacc77cbd053289f6ab20d0c2f3f5672
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-25 07:17:21 -0700

    now

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Entry point
+ * @c - to be checked
+ * Return: 1 if uppercase
+ */
+
+int _upper(int c)
+{
+if (c >=65 && c <= 90)
+return (1);
+else
+return (0)
+}
