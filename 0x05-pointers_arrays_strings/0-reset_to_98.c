9cecbd7e829938752ea727f94766134fb44137cf
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-28 19:25:24 -0700

    Reset any value of an Integer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry point
+ * *n: pointer 
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+int n = 98;
+*p = &n;
+
+printf(" the value of n is: %d", p);
+}
+
+  
