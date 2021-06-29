846a43637214d5061c88792dd64f9fc153c033bd
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-29 09:36:57 -0700

    Swaping two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#inlcude "holberton.h"
+
+/**
+ * swap_int - Entry point
+ * @a: int to swap
+ * @b: int to swap
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+int n;
+n = *a;
+*a = *b;
+*b = n;
+}
