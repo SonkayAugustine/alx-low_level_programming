619ec4fa8b73001bf308da6221f283dde707ba56
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-29 09:27:21 -0700

    Fixed bug

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,13 +4,10 @@
 /**
  * reset_to_98 - Entry point
  * @n: pointer
- * Return: Always 0.
+ * Return: n
  */
 
 void reset_to_98(int *n)
 {
-int p = 98;
-*n = &p;
-
-printf("the value of n is: %d", n);
+*n = 98;
 }
