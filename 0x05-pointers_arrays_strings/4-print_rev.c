5c299cb8aceec68df3455934e0bf4faf73db9c4d
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-06-29 10:19:29 -0700

    Adding files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_rev - function that prints in reverse
+ * fcouner is to first count to end, n is count back
+ * @s: str input
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+int fcounter = 0;
+int i, n;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+fcounter++;
+}
+
+for (n = (fcounter - 1); n >= 0; n--)
+{
+_putchar(s[n]);
+}
+_putchar('\n');
+}
