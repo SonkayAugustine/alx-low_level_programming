9159081899bd62bf0a8f16e0d22f724905101488
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-01 10:54:26 -0700

    Fixed bug

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n && src[i] '\0'; i++)
+for (i = 0; src[i] != '\0'; i++)
+if (i < n)
 dest[i] = src[i];
-for (; n > i; i++)
-dest[i] = '\0';
+while (i < n)
+dest[i++] = '\0';
 return (dest);
 }
