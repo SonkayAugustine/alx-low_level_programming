9e7026c2f49fca4e55272d54ffaa3dc321b72996
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-01 13:34:06 -0700

    Now

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,30 +3,20 @@
 /**
  * rot13 - Entry point
  * ONE if, TWO loops only...
- * @n: input
+ * @s: input
  * Return: decrypted string
  */
 
-char *rot13(char *n))
+char *rot13(char *s))
 {
-int x, rot_c = 13, i = 0;
-char toswap[] = {'A', 'N' 'a', 'n', 'B', 'O', 'o', 'C', 'P',
-'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-'s', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-'Z', 'm', 'z'};
-while (n[i] != '\0')
+char *c;
+char shift;
+
+for (c = s; *c; ++c)
 {
-for (x = 0; x <= 51; x++)
-{
-if (n[i] == toswap[x])
-{
-n[i] = n[i] + rot_c;
-x = 51;
+shift = 'A' + (*c & 32);
+if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
+*c = (*c - shift + 13) % 26 + shift;
 }
-rot_c = rot_c * -1;
-}
-i++;
-}
-return (n);
+return (s);
 }
