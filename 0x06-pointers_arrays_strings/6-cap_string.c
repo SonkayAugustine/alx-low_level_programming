12b4f30a93db8113ed8a1baf476c533bfa649e6c
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-02 08:39:05 -0700

    Capitlizing all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,11 +16,12 @@ int j;
 
 for (i = 0, j = 0; s[i]; ++i)
 {
-if ('a' <= s[i] && s[i]; <= 'z')
+if ('a' <= s[i] && s[i] <= 'z')
 {
 if (i)
 {
 for (j = 0; ws[j] && ws[j] != s[i - 1]; ++j)
+;
 }
 if (ws[j])
 s[i] -= ('a' - 'A');
