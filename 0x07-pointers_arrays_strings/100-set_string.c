729f063831099a8705d261dc5cd25cd44b3eddbd
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-06 06:39:00 -0700

    Setting the value of pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_string - Entry point
+ * @s - double pointer
+ * @to - pointer to be pointed to.
+ * Return: Always 0.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
