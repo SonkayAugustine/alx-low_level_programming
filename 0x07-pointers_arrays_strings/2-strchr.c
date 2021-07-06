151d102cc799d4ac848f26ede30b5e55af7f62d0
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-06 06:56:02 -0700

    Locate a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a char in a string
+ * @s: the string
+ * @c: char to find
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+for (; ; i++)
+{
+if (s[i] == c)
+return (&(s[i]));
+if (!s[i])
+return ('\0');
+}
+}
