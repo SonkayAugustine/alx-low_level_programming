49846c858b25d7d212765205e92a4bb2f2419717
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-07 10:20:30 -0700

    Solution for 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,6 +4,7 @@
  * _puts_recursion - A function that prints a string
  * followed by a new line.
  * @s: the pointer to the string
+ * Return: void
  */
 
 void _puts_recursion(char *s)
@@ -11,14 +12,7 @@ void _puts_recursion(char *s)
 if (*s)
 {
 _puts_recursion(s + 1);
-_putchar("%c", *s);
 }
-}
-
-/* main program to call aboe function */
-int main(void)
-{
-char a[] = " ";
-_puts_recursion(a);
-return (0);
+else
+_putchar('\n');
 }
