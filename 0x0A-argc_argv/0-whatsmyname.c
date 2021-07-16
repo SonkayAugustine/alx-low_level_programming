2b755e69321098ecc503050477dae86339044276
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-15 18:48:23 -0700

    More mallocs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,28 @@
 #include "holberton.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc - calculates the number of arguments passed.
- * @argv - arguments to be copied to argc
+ * create_array - Entry point
+ * @size - calculates the number of arguments passed.
+ * @c - arguments to be copied to argc
  * Return: 1 succesful otherwise EOF
  */
 
 
-int main (int argc, char* argv[])
+char *create_array(unsigned int size, char c)
 {
+char *ar_ptr;
+unsigned int i;
 
-
+if (size == 0)
+{
+return (NULL);
+}
+else
+{
+ar_ptr = malloc(size * sizeof(c));
+}
+for (i = 0; i < size; i++)
+*(ar_ptr + i) = c;
+return (ar_ptr);
 }
