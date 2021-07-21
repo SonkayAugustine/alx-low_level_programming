996c81200fea9ba5f4a3528cf53362371dc9f261
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-21 10:07:34 -0700

    Added protoypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,10 @@ char *owner;
  */
 typedef struct dog dog_p;
 
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _putchar(char c);
 #endif /* DOG */
