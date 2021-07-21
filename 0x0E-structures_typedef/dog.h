b410f7d6afdae484c8547a6c9921f8eb7309189f
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-21 09:53:26 -0700

    Solution for 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,24 @@
-#include <stdio.h>
+#ifndef _DOG_H_
+#define _DOG_H_
 
+/**
+ * struct dog - Creates a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner's name of the dog
+ *
+ * Description: Creates a dog
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
-}
+};
+
+/**
+ * dog_p - Typedef for struct dog
+ */
+typedef struct dog dog_p;
+
+#endif /* DOG */
