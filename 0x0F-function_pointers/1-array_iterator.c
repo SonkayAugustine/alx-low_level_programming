318c774bc2131065f78274711cfe5d09a5c25c6e
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-21 19:33:51 -0700

    Solution for 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - Iterates over an array and calls a function
+ * @array: Array
+ * @size: size
+ * @action: pointer to a function
+ *
+ * return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i = 0;
+if (size <= 0 || array == NULL || action == NULL)
+return;
+
+for (i = 0; i < size; i++)
+action(*(array + i));
+}
