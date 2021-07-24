3c19e7070da6d3211d02976f2a4ab3203c400a25
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-23 18:40:15 -0700

    Solution for 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,7 +7,7 @@
  * @n: amount of arguments in list
  */
 
-void print_strings(const char *separator, const unsigne int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list valist;
 unsigned int i;
