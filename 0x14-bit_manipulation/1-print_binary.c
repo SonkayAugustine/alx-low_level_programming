2d33cd9b513c20304a6e0335b9177ca0a8c743bf
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-08-06 11:57:03 -0700

    Fixed task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,15 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-int k = 0, i = 0, start = 0;
-for (i = 63; i >= 0; i--)
-{
-k = n >> i;
-if (k & 1)
-start = 1;
-if ((k & 1) && start)
-_putchar('1');
-else if (start)
-_putchar('0');
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
-}
+
