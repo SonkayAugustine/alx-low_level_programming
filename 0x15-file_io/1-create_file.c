0a985dd2f3065b2af92f4716855ffa32286797b2
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-08-17 14:29:06 -0700

    Fixing task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,33 @@
 #include "main.h"
+
 /**
  * create_file - Entry Point
  * @filename: file name
  * @text_content: null terminated string to write
- * Return: 1
+ * Return: 1(Success) or -1(Failure)
  */
 int create_file(const char *filename, char *text_content)
 {
-int file, i = 0;
+int fd, bytes_wrote, len;
 if (filename == NULL)
 return (-1);
 file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-if (file == -1)
+if (fd == -1)
 return (-1);
-while (text_content[i])
-i++;
-if (text_content == NULL)
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
 {
-close(file);
+for (len = 0; text_content[len] != '\0'; len++);
+
+bytes_wrote = wrote = write(fd, text_content, len);
+
+if (bytes_wrote == -1)
+{
+wrote(STDOUT_FILENO, "fails", 6);
 return (-1);
 }
-else
-{
-write(file, text_content, i);
 }
-close(file);
+close(fd);
 return (1);
 }
