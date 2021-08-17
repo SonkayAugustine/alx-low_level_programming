bd8920038ea8d7b57cb674b9ca1dce0b59168d21
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-08-17 14:41:21 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+/* dprintf */
 #include <stdio.h>
+/* malloc, free */
 #include <stdlib.h>
+/* close */
 #include <unistd.h>
+/* open */
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,6 +16,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int cpy_file(int fd_src, int fd_dest, char *name_source, char*name_dest);
 
 #endif /* MAIN_H */
