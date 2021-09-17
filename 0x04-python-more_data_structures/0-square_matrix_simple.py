a7f463b87b192fd26598590b740970f33aef89cb
Sonkay <51463111+SonkayAugustine@users.noreply.github.com>
2021-09-17 16:29:21 +0000

    d

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    sqr_matrix = []
-    for row in matrix:
-        row = list(map(lambda x: x**2, row))
-        sqr_matrix.append(row)
-    return sqr_matrix
--- 0x04-python-more_data_structures/5-number_keys.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def number_keys(a_dictionary):
-    num_key = len(a_dictionary)
-    return num_key
