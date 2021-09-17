2108c1483d5abbdbf1f8e7e40523cfdeed8dd7c6
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-09-17 16:44:29 -0700

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,20 +10,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new_node;
 new_node = malloc(sizeof(dlistint_t));
-if (!new_node == NULL)
+if (new_node == NULL)
 return (NULL);
-if (*head == NULL)
-{
-new_node->next = NULL;
-new_node->prev = NULL;
+
 new_node->n = n;
-(*head) = new_node;
-return (*head);
-}
 new_node->prev = NULL;
 new_node->next = *head;
-new_node->n = n;
-(*head)->prev = new_node;
-(*head) = new_node;
-return (*head);
+*head = new_node;
+return (new_node);
 }
