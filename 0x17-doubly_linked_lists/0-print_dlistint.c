b3b5866703dce7fe13c21fdb531e851370ca2788
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-09-17 07:48:18 -0700

    Fixed betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,16 +8,16 @@
 size_t print_dlistint(const dlistint_t *h)
 
 {
-  const dlistint_t *tmp;
-  size_t count;
- 
-  tmp = h;
-  count = 0;
-  while (tmp != NULL)
-    {
-      printf("%d\n", tmp->n);
-      count++;
-      tmp = tmp->next;
-    }
-  return (count);
+const dlistint_t *tmp;
+size_t count;
+
+tmp = h;
+count = 0;
+while (tmp != NULL)
+{
+printf("%d\n", tmp->n);
+count++;
+tmp = tmp->next;
+}
+return (count);
 }
