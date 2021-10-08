e239f747765a4a8d29f333a1e4d6e976b3c6ea3d
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-10-08 15:22:38 -0700

    Upgrade README

--- 0xA1-hash_tables/0-hash_table_create.c
+++ 0xA1-hash_tables/0-hash_table_create.c
@@ -6,16 +6,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_table;
+hash_table_t *ht;
 
-if (size == 0)
+if (size < 1)
 return (NULL);
-hash_table = malloc(sizeof(hash_table_t));
-if (hash_table == NULL)
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
 return (NULL);
-hash_table->size = size;
- hash_table->array = calloc(size, sizeof(hash_node_t ));
-if (hash_table->array == NULL)
+ht->size = size;
+ht->array = calloc(size, sizeof(hash_node_t ));
+if (ht->array == NULL)
+{
+free(ht);
 return (NULL);
-return (hash_table);
+}
+ return (ht);
 }
