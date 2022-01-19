acdad6b0b9b0b9ce47a5bc6c434587d1ac876e30
SonkayAugustine <zamanisonkay@gmail.com>
2022-01-19 05:38:23 -0800

    fixed header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -16,15 +16,16 @@
  */
 typedef struct listint_s
 {
-  int n;
-  size_t index;
-  struct listint_s *next;
+int n;
+size_t index;
+struct listint_s *next;
 } listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
  * @n: Integer
+ * @index: index of node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
  *
@@ -33,10 +34,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-  int n;
-  size_t index;
-  struct skiplist_s *next;
-  struct skiplist_s *express;
+int n;
+size_t index;
+struct skiplist_s *next;
+struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
