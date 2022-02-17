acb6f86517ac683f50301684517b15397a25be2e
SonkayAugustine <zamanisonkay@gmail.com>
2022-02-17 15:03:43 +0000

    fixed betty err

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
  * main - Entry point
  * @argc: calculates the number of arguments passed.
  * @argv: arguments to be copied to argc
- * 
+ *
  * Return: 0 succesful otherwise EOF
  */
 
 int main(int argc __atrribute__((unused)), char *argv[])
 {
-  printf("%s\n", argv[0]);
-  return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
