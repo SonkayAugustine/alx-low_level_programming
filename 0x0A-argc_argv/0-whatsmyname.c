e6507934beb0fb07d88c58ae6829a8ea821236a0
SonkayAugustine <zamanisonkay@gmail.com>
2022-02-17 14:38:27 +0000

    redo tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,28 +1,16 @@
-#include "holberton.h"
-#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
 
 /**
- * create_array - Entry point
- * @size - calculates the number of arguments passed.
- * @c - arguments to be copied to argc
- * Return: 1 succesful otherwise EOF
+ * main - Entry point
+ * @argc: calculates the number of arguments passed.
+ * @argv: arguments to be copied to argc
+ * 
+ * Return: 0 succesful otherwise EOF
  */
 
-
-char *create_array(unsigned int size, char c)
+int main(int argc __atrribute__((unused)), char *argv[])
 {
-char *ar_ptr;
-unsigned int i;
-
-if (size == 0)
-{
-return (NULL);
-}
-else
-{
-ar_ptr = malloc(size * sizeof(c));
-}
-for (i = 0; i < size; i++)
-*(ar_ptr + i) = c;
-return (ar_ptr);
+  printf("%s\n", argv[0]);
+  return (0);
 }
