9d4e9924bde373b5d43c42b9450a32d49e4998e5
SonkayAugustine <zamanisonkay@gmail.com>
2022-06-14 20:30:23 +0000

    Fixed err

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 /**
  * read_textfile - Entry Point
  * @filename: file name
@@ -7,24 +13,32 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file, rd, wr;
+int file;
+ssize_t lenr, lenw;
+
 char *buf;
+
 if (filename == NULL)
 return (0);
 file = open(filename, O_RDONLY);
 if (file == -1)
 return (0);
-buf = malloc(sizeof(char) * letters + 1);
+buf = malloc(sizeof(char) * letters);
 if (buf == NULL)
-return (0);
-rd = read(file, buf, letters);
-if (rd == -1)
-return (0);
-buf[letters] = '\0';
-wr = write(1, buf, rd);
-if (wr == -1)
-return (0);
+{
 close(file);
+return (0);
+}
+lenr = read(file, buf, letters);
+close(file);
+if (lenr == -1)
+{
 free(buf);
-return (wr);
+return (0);
+}
+lenw = write(STDOUT_FILENO, buf, lenr);
+free(buf);
+if (lenr != lenw)
+return (0);
+return (lenw);
 }
