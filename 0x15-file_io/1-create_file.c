0c2a32ac3c14a65f6fadeeb4657e02bed8b201c2
SonkayAugustine <zamanisonkay@gmail.com>
2022-06-14 20:49:20 +0000

    fixed 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,23 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * _strlen - finds the length of string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
+ */
+size_t _strlen(char *str)
+{
+size_t i;
+
+for (i = 0; str[i]; i++)
+;
+return (i);
+}
 
 /**
  * create_file - Entry Point
@@ -8,22 +27,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, bytes_wrote, len;
+int fd;
+ssize_t len = 0;
+
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+fd = open(filename, 0-WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
-{
-for (len = 0; text_content[len] != '\0'; len++)
-;
-
-bytes_wrote = write(fd, text_content, len);
-
-if (bytes_wrote == -1)
-{
-write(STDOUT_FILENO, "fails", 6)
+len = write(fd, text_content, _strlen(text_content));
+if (len == -1)
+return (-1);
 return (-1);
 }
 
