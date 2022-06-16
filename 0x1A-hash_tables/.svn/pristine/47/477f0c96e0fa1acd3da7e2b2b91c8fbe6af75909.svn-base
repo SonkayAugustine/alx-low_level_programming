7760b634ad10600db7f5f87c035f679efe67e734
SonkayAugustine <zamanisonkay@gmail.com>
2022-06-16 03:23:19 +0000

    fixed err

---
+++ 0x1A-hash_tables/.svn/pristine/47/477f0c96e0fa1acd3da7e2b2b91c8fbe6af75909.svn-base
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_get - get item from hash table with a given key
+  * @ht: hash table
+  * @key: key to corresponding item
+  * Return: NULL if fail, pointer to value on success
+  */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int indexKey;
+	hash_node_t *pointer;
+
+	if (!ht || !key)
+		return (NULL);
+	/* get index */
+	indexKey = key_index((const unsigned char *)key, ht->size);
+
+	pointer = ht->array[indexKey];
+	if (!pointer)
+		return (NULL);
+
+	while ((pointer && pointer->key) && strcmp(pointer->key, key) != 0)
+	{
+		pointer = pointer->next;
+	}
+
+	return (pointer->value);
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_set - insert item in a key value hash table
+ * @ht: hash table
+ * @key: const char *
+ * @value: const char *
+ * Return: 1 if success, 0 if fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newElement, *walk;
+	unsigned long int tableIndex;
+
+	if (!key || !*key || !value || !ht)
+		return (0);
+	tableIndex = key_index((const unsigned char *) key, ht->size);
+	walk = ht->array[tableIndex];
+	while (walk)
+	{
+		if (strcmp(key, walk->key) == 0)
+		{
+			free(walk->value);
+			walk->value = strdup(value);
+			if (!walk->value)
+				return (0);
+			return (1);
+		}
+		walk = walk->next;
+	}
+
+	newElement = malloc(sizeof(hash_node_t));
+	if (!newElement)
+		return (0);
+	newElement->key = strdup(key);
+	newElement->value = strdup(value);
+	if (newElement->value == NULL)
+	{
+		free(newElement);
+		return (0);
+	}
+	newElement->next = ht->array[tableIndex];
+	ht->array[tableIndex] = newElement;
+	return (1);
+}
